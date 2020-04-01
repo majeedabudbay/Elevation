fc5dd3bdf7da6a1ee684f64c887fcc70dcd03c82
majeedabudbay <48244398+majeedabudbay@users.noreply.github.com>
2020-04-01 13:47:18 +0300

    Add Files

---
+++ Week1/Day2/JS/main.js
@@ -0,0 +1,5 @@
+const myVariable = "The potential of the future "+ "is the best thing ever" // notice the space we're leaving at the end
+
+//const result = myVariable 
+
+console.log(myVariable) // logs "The potential of the future is the best thing ever"
---
+++ Week1/Day2/Variables & Operators/variables.js
@@ -0,0 +1,11 @@
+//Exercise 1
+
+const maxCapacity = 14 //14
+let surfTime = true //true
+let bestStudent //undefined
+//const purposeInLife //missing initializer
+
+console.log(maxCapacity)
+console.log(surfTime)
+console.log(bestStudent)
+//console.log(purposeInLife)
